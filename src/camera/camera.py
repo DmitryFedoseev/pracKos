0cb9a43d15fe33f2b148ae2620e4463a2d86e3cf
DmitryFedoseev <71728626+DmitryFedoseev@users.noreply.github.com>
2022-07-05 09:44:20 +0300

    Add files via upload

--- src/camera/camera.py
+++ src/camera/camera.py
@@ -8,18 +8,37 @@ video = cv.VideoCapture(0)
 
 while True:
     success, img = video.read()
-    image = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    mask = cv.inRange(image, lower, upper)
+    try:
+        image = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        mask = cv.inRange(image, lower, upper)
 
-    contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        moments = cv.moments(mask, 1)
+        dM01 = moments['m01']
+        dM10 = moments['m10']
+        dArea = moments['m00']
 
-    if len(contours) != 0:
-        for contour in contours:
-            if cv.contourArea(contour) > 500:
-                x, y, width, height = cv.boundingRect(contour)
-                cv.rectangle(img, (x, y), (x + width, y + height), (0, 0, 255), 3)
+        contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+        if len(contours) != 0:
+            for contour in contours:
+                if cv.contourArea(contour) > 500:
+                    x, y, width, height = cv.boundingRect(contour)
+                    cv.rectangle(img, (x, y), (x + width, y + height), (0, 0, 255), 3)
+
+        if dArea > 150:
+            x = int(dM10 / dArea)
+            y = int(dM01 / dArea)
+            cv.circle(img, (x, y), 4, (0, 0, 255), -1)
+
+    except:
+        video.release()
+        raise
 
     cv.imshow("mask", mask)
     cv.imshow("webcam", img)
 
-    cv.waitKey(1)
+    k = cv.waitKey(1)
+    if k == 27:
+        break
+
+cv.destroyAllWindows()
