3e9ae7699a685368fba9cd62ecabd26575fe32af
DmitryFedoseev <71728626+DmitryFedoseev@users.noreply.github.com>
2022-07-06 04:55:01 +0300

    fixed
    Add find coordinates

--- src/camera/camera.py
+++ src/camera/camera.py
@@ -2,43 +2,43 @@ import cv2 as cv
 import numpy as np
 
 lower = np.array([15, 150, 20])
-upper = np.array([35,255,255])
+upper = np.array([35, 255, 255])
 
-video = cv.VideoCapture(0)
-
-while True:
-    success, img = video.read()
-    try:
+def find_object(video):
+    while True:
+        success, img = video.read()
         image = cv.cvtColor(img, cv.COLOR_BGR2HSV)
         mask = cv.inRange(image, lower, upper)
-
-        moments = cv.moments(mask, 1)
-        dM01 = moments['m01']
-        dM10 = moments['m10']
-        dArea = moments['m00']
 
         contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
         if len(contours) != 0:
             for contour in contours:
-                if cv.contourArea(contour) > 500:
-                    x, y, width, height = cv.boundingRect(contour)
-                    cv.rectangle(img, (x, y), (x + width, y + height), (0, 0, 255), 3)
+                area = cv.contourArea(contour)
+                if area > 500:
+                    x, y, w, h = cv.boundingRect(contour)
+                    cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
+                    rect = cv.minAreaRect(contour)
+                    box = cv.boxPoints(rect)
+                    box = np.int0(box)
+                    center = (int(rect[0][0]), int(rect[0][1]))
+                    area = cv.contourArea(contour)
+                    cv.circle(img, center, 2, (0, 0, 255), 2)
+                    text = "(" + str(center[0]) + ", " + str(center[1]) + ")"
+                    cv.putText(img, text, (center[0] + 10, center[1] + 10), cv.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 0),
+                                1, 8, 0)
 
-        if dArea > 150:
-            x = int(dM10 / dArea)
-            y = int(dM01 / dArea)
-            cv.circle(img, (x, y), 4, (0, 0, 255), -1)
+        cv.imshow("mask", mask)
+        cv.imshow("webcam", img)
 
-    except:
-        video.release()
-        raise
+        k = cv.waitKey(1)
+        if k == 27:
+            break
 
-    cv.imshow("mask", mask)
-    cv.imshow("webcam", img)
 
-    k = cv.waitKey(1)
-    if k == 27:
-        break
+if __name__ == '__main__':
+    video = cv.VideoCapture(0)
+    find_object(video)
 
-cv.destroyAllWindows()
+    video.release()
+    cv.destroyAllWindows()
