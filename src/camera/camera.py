08948f50446d7a2313a66702efcaa185099e26d0
Nik148 <rogov_ni@mail.ru>
2022-07-07 10:16:48 +0300

    save coordinats in file

--- src/camera/camera.py
+++ src/camera/camera.py
@@ -4,6 +4,11 @@ import numpy as np
 lower = np.array([15, 150, 20])
 upper = np.array([35, 255, 255])
 
+def write_file(cent):    
+    file = open("/home/nikita/Progi/test/coords.txt", "a")
+    file.write(str(cent) + '\n')
+    file.close()
+
 def find_object(video):
     while True:
         success, img = video.read()
@@ -13,6 +18,8 @@ def find_object(video):
         contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
         if len(contours) != 0:
+            array = [0] * len(contours)
+            count = 0
             for contour in contours:
                 area = cv.contourArea(contour)
                 if area > 500:
@@ -22,6 +29,8 @@ def find_object(video):
                     center = (int(rect[0][0]), int(rect[0][1]))
                     cv.circle(img, center, 2, (0, 0, 255), 2)
                     text = "(" + str(center[0]) + ", " + str(center[1]) + ")"
+                    array[count] = text
+                    count += 1
                     cv.putText(img, text, (center[0] + 10, center[1] + 10), cv.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 0), 1, 8, 0)
 
         cv.imshow("mask", mask)
@@ -29,6 +38,9 @@ def find_object(video):
 
         k = cv.waitKey(1)
         if k == 27:
+            for coordinat in array:
+                if coordinat != 0:
+                    write_file(coordinat)
             break
 
 
